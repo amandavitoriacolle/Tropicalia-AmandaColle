5f0f3a33283ab7e65fa5e9c79efec6d407a0cc46
amandavitoriacolle <145784896+amandavitoriacolle@users.noreply.github.com>
2025-04-01 08:26:25 -0300

    5

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ document.addEventListener('DOMContentLoaded' , function (){
 
     let tamanhoAtualFonte = 1;
 
-    
-}
+    AumentaFonteBotão. addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize =`${tamanhoAtualFonte}rem`; 
+    });
+};
